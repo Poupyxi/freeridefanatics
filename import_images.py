1e3ae90add7b79b3facb537d672a1668fd0e9dbe
greteau marc <greteau.m@gmail.com>
2026-08-04 17:44:25 +0200

    Restore Finn Iles equipment photos

--- import_images.py
+++ import_images.py
@@ -85,9 +85,13 @@ FOLDER_TO_CATEGORY = {
 PHOTO_ALIASES = {
     ("Frame", "norco", "prototype dh"): ("norco", "torrent dh"),
     ("Frame", "norco", "dh prototype"): ("norco", "torrent dh"),
+    ("Frame", "specialized", "demo"): ("s works", "demo"),
+    ("RearShock", "ohlins", "prototype coil"): ("ohlins", "ttx22 m2 coil"),
     ("Handlebar", "cast components", "20mm rise"): ("cast", "sfx"),
     ("Crankset", "shimano", "saint"): ("shimano", "shimano saint fc m825 single 10 speed crankset"),
+    ("BrakeLever", "brembo lever", ""): ("brembo gr pro gravity lever", ""),
     ("BrakeLever", "shimano", "xtr m9120"): ("shimano", "xtr 9120"),
+    ("Wheels", "dt swiss", "ex471 + dt swiss 240"): ("dtswiss", "fanatik"),
 }
 
 # ---------------------------------------------------------------- helpers
